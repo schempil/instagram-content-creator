e16148b718705ea8023ce67ac3e8dca0b8f67fc5
Philipp Schemel <philippschemel@live.com>
2020-11-12 11:25:21 +0100

    Fix request issue with piping the response data

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,10 +1,13 @@
 import { HttpService, Injectable } from '@nestjs/common';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class PostService {
   constructor(private httpService: HttpService) {}
 
   getRandomPost() {
-    return this.httpService.get('https://jsonplaceholder.typicode.com/todos/1');
+    return this.httpService
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .pipe(map((response) => response.data));
   }
 }
